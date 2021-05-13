750ddbbdb6ae769eaf77b5daddb5b01359663e59
Fosrey <fosrey@gmail.com>
2021-05-13 18:42:52 -0500

    Traduccion Final y MOCK
    Agregados los campos requeridos por el profesor

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,17 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: "Superman", edad: 30, poder: "Superfuerza, volar", identidad: "Clark Ken", universo: "DC" },
+      { id: 12, name: "Batman", edad: 27, poder: "Traje de murcielago", identidad: "Bruno Diaz", universo: "DC" },
+      { id: 13, name: "Hulk", edad: 40, poder: "Transformacion", identidad: "Bruce Banner", universo: "Marvel" },
+      { id: 14, name: "Ironman", edad: 28, poder: "Traje Tecnologico", identidad: "Tony Stark", universo: "Marvel" },
+      { id: 15, name: "Thor", edad: 1000, poder: "Manipular Truenos y Rayos", identidad: "Thor", universo: "Marvel" },
+      { id: 16, name: "Spiderman", edad: 20, poder: "Telaraña, Sentido Aracnido", identidad: "Peter Parker", universo: "Marvel" },
+      { id: 17, name: "Black Widow", edad: 33, poder: "Fuerza Superior", identidad: "Natasha Romanof", universo: "Marvel" },
+      { id: 18, name: "Flash", edad: 24, poder: "Velocidad del sonido", identidad: "Barry Allen", universo: "DC" },
+      { id: 19, name: "Bruja escarlata", edad: 29, poder: "Magia", identidad: "Wanda Maximoff", universo: "Marvel" },
+      { id: 20, name: "Wonder Woman", edad: 26, poder: "Amazonico", identidad: "Diana Prince", universo: "DC" },
+
     ];
     return {heroes};
   }
@@ -31,10 +32,3 @@ export class InMemoryDataService implements InMemoryDbService {
     return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
